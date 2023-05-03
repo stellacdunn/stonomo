d33c5c7f47d082536f4ea4c7792ee70529544446
Stella Dunn <stellacdunn@gmail.com>
2023-05-03 13:07:15 -0600

    Fixed ObjectId type references. Added launch.json

--- server/evictionModel.js
+++ server/evictionModel.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose'
 
 export { Eviction, addEviction /*, updateEviction */ }
 
-var Object_ID = mongoose.Schema.Object_ID
+var ObjectId = mongoose.Schema.Types.ObjectId
 
 //TODO: add validation and error handling
 const evictionsSchema = new mongoose.Schema({
-	_tenant: {type: Object_ID, ref: 'Tenant', required: true, index: true},
-	_facility: {type: Object_ID, ref: 'Facility', required: true, index: true},
-	_user: {type: Object_ID, ref: 'User', required: true, index: true},
-	_reason: {type: Object_ID, ref: 'Reason', required: true, index: true},
+	_tenant: {type: ObjectId, ref: 'Tenant', required: true, index: true},
+	_facility: {type: ObjectId, ref: 'Facility', required: true, index: true},
+	_user: {type: ObjectId, ref: 'User', required: true, index: true},
+	_reason: {type: ObjectId, ref: 'Reason', required: true, index: true},
 	details: {type: [String], required: true}, //TODO: convert to nested document with timestamps for each entry
 	evicted: {type: Date, required: true}
 }, {timestamps: true})
--- server/userModel.js
+++ server/userModel.js
@@ -2,15 +2,15 @@ import mongoose from 'mongoose'
 
 export { User, addUser /*, modifyUser, deleteUser */}
 
-var Object_ID = mongoose.Schema.Object_ID
-
-const User = mongoose.model("Users", userSchema)
+var ObjectId = mongoose.Schema.Types.ObjectId
 
 const userSchema = mongoose.Schema({
 	username: {type: String, required: true},
 	pass_hash: {type: String, required: true},
-	_facility: {type: Object_ID, ref: 'Facility', required: true}
+	_facility: {type: ObjectId, ref: 'Facility', required: true}
 })
+
+const User = mongoose.model("Users", userSchema)
 
 const addUser = async(username, pass_hash, _facility) => {
 	let user = new User(username, pass_hash, _facility)
