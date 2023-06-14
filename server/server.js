c60c197851adc484ffe2acb8373fdc58202e13df
Stella Dunn <stellacdunn@gmail.com>
2023-06-14 14:55:41 -0600

    Fixed param handler issue

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ console.log('Stonomo Server starting - ' + Date.now());
 
 //global consts
 const PORT = 8080;
-const app = express();
 
 console.log('Connecting to Mongo DB');
 const connection = 'mongodb://db:27017/stonomo';
@@ -32,34 +31,38 @@ mongoose.connect(connection, connOpts).then(() => {
 
 console.log('Setting up Express server');
 
+const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-router.param('user', function (req, res, next, id) {
-    // try to get the user details from the User model and attach it to the request object
+console.log('Setting up route and param handlers');
+
+app.param('user', function (req, res, next, id) {
+// try to get the user details from the User model and attach it to the request object
     User.find(id, function (err, user) {
       if (err) {
-        next(err)
+        next(err);
       } else if (user) {
-        req.user = user
-        next()
+        req.user = user;
+        next();
       } else {
-        next(new Error('failed to load user'))
+        next(new Error('failed to load user'));
       }
-    })
-  })
+    });
+  });
 
 app.get('/', (req, res) => {
     console.log('connecting to route route "/"');
     res.status(200).send('Stonomo server');
 });
 
-app.post('/search', (req, res, next) => {
-    const { fname, mname, lname, street1, street2, city, state, zip, birthdate, email, phone, ...otherFields } = req.body;
+app.post('/search', async(req, res, next) => {
+    const { fname, mname, lname, street1, street2, street3, city, state, zip, birthdate, email, phone, ...otherFields } = req.body;
 
-    //use Tenant.find to search for people
+    //use Tenant.find() to search for people
+    let tenantMatch = await Tenant.find(req.body); //??
     //Eviction.find({tenant: Tenant.find...})
 
     req.on('error', printErrorAndNext(next));
